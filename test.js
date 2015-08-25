a4d2a1c52670b25eb87ec0fcedfdc8310288ea2e
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-25 20:46:42 -0300

    Fixed and added code annotations to the test file.

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 'use strict';
 
-var generator = require('./');
+// TODO: Take a look at `slush-angular` test suite: https://github.com/slushjs/slush-angular/blob/master/test/slush-angular_test.js
+
 var test = require('tape');
 var gulp = require('gulp');
+
+require('./slushfile');
